d837f49682bf756e7e4ddd8a04023b6bfea82aab
Jimimimi <jimmy.messinis@gmail.com>
2014-07-27 19:27:50 +0200

    load city markers

--- components/game/engine.js
+++ components/game/engine.js
@@ -1,7 +1,7 @@
 (function(){
 
-angular.module('letsrule-engine',['letsrule-db','ui-devlog','letsrule-models'])
-.service('engine', ['devlog','Country',function($log,Country){
+angular.module('letsrule-engine',['letsrule-db','ui-devlog','letsrule-models','leaflet-directive'])
+.service('engine', ['devlog','Country','leafletData',function($log,Country,leafletData){
   var engine = window.engine = this;
 
   engine.loadDb = function(db){
@@ -10,6 +10,16 @@ angular.module('letsrule-engine',['letsrule-db','ui-devlog','letsrule-models'])
     $log.addEvent('Game engine loaded')
     engine.country = new Country(db.data.countries[0]);
     
+    var cityLayer = L.layerGroup();
+    engine.country.regions.forEach(function(region){
+      region.cities.forEach(function(city){
+        cityLayer.addLayer(L.marker(city.geo));
+      })
+    })
+
+    leafletData.getMap().then(function (map) {
+      cityLayer.addTo(map);
+    });
   };
 
 
